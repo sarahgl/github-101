32e28b83779c4aea971354360717af4f51c624f4
sarandongah <sarah.llebres@gmail.com>
2024-02-29 20:04:18 +0100

    Anuario

---
+++ github-101/2-exploring-repos/12-yearbook.sh
@@ -0,0 +1,15 @@
+# codedex-io
+# github-101
+# Anuario
+
+# Bifurcar https://github.com/codedex-io/github-chapter-2-contributions
+
+# Clonar a repositorio local
+git clone https://github.com/sarahgl/github-chapter-2-contributions.git
+
+# Antes de realizar los cambios
+git add .
+git commit -m "Anuario"
+git push -u origin main
+
+# Crear una sugerencia de fusión para aplicar los cambios en la rama 'main' del repositorio original
