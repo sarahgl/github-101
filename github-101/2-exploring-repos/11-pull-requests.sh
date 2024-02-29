fd67709491f01aa785a8fdf232b3a5b081938166
sarandongah <sarah.llebres@gmail.com>
2024-02-29 19:52:49 +0100

    Sugerir cambios

---
+++ github-101/2-exploring-repos/11-pull-requests.sh
@@ -0,0 +1,17 @@
+# codedex-io
+# github-101
+# Sugerir cambios
+
+# Confirmamos que estamos en la rama 'main'
+git branch
+
+# Cambiamos a 'nueva rama' 
+git checkout sarahgl/edit
+
+# Preparar, confirmar y subir los cambios a la rama equivalente del repositorio remoto
+git add .
+commit -m "Sugerir cambios"
+git push -u origin sarahgl/edit
+
+# Fusionar los cambios sugeridos 
+# https://github.com/sarahgl/github-chapter-2-sandbox/tree/sarahgl/edit
